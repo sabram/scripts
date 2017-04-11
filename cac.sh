6d0e2a49457ecb366123a458d0538d291b8f41b4
sabram <sabram@lendingclub.com>
2017-04-11 13:02:45 -0700

    Update cac.sh script

--- cac.sh
+++ cac.sh
@@ -1,9 +1,18 @@
-//Creates, Adds and Commits a file
-//if you call with parameter foo, it will create a file called foo containing text foo
-//and commit it to the current git branch
+# Creates, Adds and Commits a file
+# If you call with parameter foo, it will create a file called foo containing text foo
+# and commit it to the current git branch
+# Why? Useful when experimenting with Git and I just want to add and commit a file for testing.
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1
+fi
+
 FILE=$1
 touch "$FILE"
 echo "$FILE" > ./$FILE
 
 git add $FILE
 git commit -m "added file: $FILE"
+echo "Created, added and committed file: $FILE"
